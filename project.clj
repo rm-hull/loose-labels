0f049b0c3e7672752c439fa4a2ba553c0fa5e93c
Richard Hull <rm_hull@yahoo.co.uk>
2017-06-22 08:49:27 +0100

    Update dependencies

--- project.clj
+++ project.clj
@@ -20,5 +20,5 @@
         [org.clojure/clojure "1.8.0"]
         [org.clojure/test.check "0.9.0"]]
       :plugins [
-        [lein-cloverage "1.0.7"]
-        [lein-codox "0.10.0"]]}})
+        [lein-cloverage "1.0.9"]
+        [lein-codox "0.10.3"]]}})
